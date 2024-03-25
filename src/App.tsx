16a7794d3d8c7275f9c0a5d034188587cb9e5fa5
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-03-25 23:17:46 +0100

    Update example text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const colorBg = theme === 'light' ? 'bg-lightBG' : 'bgdeepestBlack';
   return (
     <main className={`font-spartan font-900 ${colorBg} flex h-dvh grow-0`}>
-      <h1>Example text</h1>
+      <h1>Example text 2</h1>
     </main>
   );
 }
