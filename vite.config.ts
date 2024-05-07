35789074170d7717b45797d1231f6d63529d9899
dmitinev <mitinov.dmitriy@gmail.com>
2024-05-07 13:28:21 +0200

    Add LoadingPage and update vite build configuration
    Added a new LoadingPage with corresponding styling module. Updated TypeScript configuration to include ES2021.String. Updated vite.config.ts build configuration for effective chunking of several libraries. Converted the import of InvoiceDetailsPage to a lazy-loaded component, and used the LoadingPage as the fallback in App.tsx.

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,32 @@ export default defineConfig({
       assets: '/src/assets',
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // @ts-expect-error 'string has includes method'
+          if (id.includes('react-select')) {
+            return '@react-select';
+          }
+          // @ts-expect-error 'string has includes method'
+          if (id.includes('react-datepicker')) {
+            return '@react-datepicker';
+          }
+          if (
+            // @ts-expect-error 'string has includes method'
+            id.includes('react-router-dom') ||
+            // @ts-expect-error 'string has includes method'
+            id.includes('@remix-run') ||
+            // @ts-expect-error 'string has includes method'
+            id.includes('react-router')
+          ) {
+            return '@react-router';
+          }
+        },
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
